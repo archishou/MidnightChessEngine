7830ed5bae54e0d3ad70b28114df6f0ee08c635f
Archishmaan <archishou@gmail.com>
2023-02-25 23:27:23 -0500

    refactor evaluate with Score struct

--- ChessEngine/ChessEngine_lib/evaluation/evaluate.h
+++ ChessEngine/ChessEngine_lib/evaluation/evaluate.h
@@ -3,29 +3,29 @@
 #include "pawn.h"
 
 template<Color color>
-void compute_piece_score(Position& board, PieceType piece_type, int& mg_score, int& eg_score, int& game_phase) {
+constexpr void compute_piece_score(Position& board, PieceType piece_type, Score& score, int& game_phase) {
 	Bitboard piece_bitboard_us = board.bitboard_of(color, piece_type);
 	while (piece_bitboard_us) {
 		Square square = pop_lsb(&piece_bitboard_us);
-		mg_score += read_pstq<color>(MIDDLE, piece_type, square) + PIECE_VALUES[MIDDLE][piece_type];
-		eg_score += read_pstq<color>(END, piece_type, square) + PIECE_VALUES[END][piece_type];
+		score += read_pstq<color>(piece_type, square) + PIECE_VALUES[piece_type];
 		game_phase += GAME_PHASE_BONUS[piece_type];
 	}
 }
 
 template<Color color>
-int evaluate_all_piece_positions(Position& board) {
-	int mg_score_us = 0, eg_score_us = 0;
-	int mg_score_them = 0, eg_score_them = 0;
+constexpr int evaluate_all_piece_positions(Position& board) {
+	Score us = Score(), them = Score();
 	int game_phase = 0;
 	for (PieceType piece_type : {PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING}) {
-		compute_piece_score<color>(board, piece_type, mg_score_us, eg_score_us, game_phase);
-		compute_piece_score<~color>(board, piece_type, mg_score_them, eg_score_them, game_phase);
+		compute_piece_score<color>(board, piece_type, us, game_phase);
+		compute_piece_score<~color>(board, piece_type, them, game_phase);
 	}
-	int mg_score = mg_score_us - mg_score_them;
-	int eg_score = eg_score_us - eg_score_them;
+
 	int mg_phase = std::min(game_phase, 24);
 	int eg_phase = 24 - mg_phase;
+
+	const int mg_score = us.middle_game - them.middle_game;
+	const int eg_score = us.end_game - them.end_game;
 	return (mg_score * mg_phase + eg_score * eg_phase) / 24;
 }
 
---
+++ ChessEngine/ChessEngine_lib/evaluation/types.h
@@ -0,0 +1,53 @@
+//
+// Created by Archishmaan Peyyety on 2/25/23.
+//
+
+
+constexpr int NGAME_PHASES = 2;
+enum GamePhase {
+	MIDDLE,
+	END
+};
+
+struct Score {
+	short middle_game;
+	short end_game;
+	constexpr Score (short m, short e) : middle_game(m), end_game(e) {}
+	constexpr Score () : middle_game(0), end_game(0) {}
+	Score& operator = (const Score& s) = default;
+	constexpr Score operator + (const Score& s) const {
+		return {
+			(short) (middle_game + s.middle_game),
+			(short) (end_game + s.end_game)
+		};
+	}
+	constexpr Score operator * (const short& s) const {
+		return {
+				(short) (middle_game * s),
+				(short) (end_game * s)
+		};
+	}
+	constexpr Score operator - (const Score& s) const {
+		return {
+				(short) (middle_game - s.middle_game),
+				(short) (end_game - s.end_game)
+		};
+	}
+	inline Score& operator += (const Score& s) {
+		middle_game = short(middle_game + s.middle_game);
+		end_game = short(end_game + s.end_game);
+		return *this;
+	}
+	inline Score& operator -= (const Score& s) {
+		middle_game = short(middle_game - s.middle_game);
+		end_game = short(end_game - s.end_game);
+		return *this;
+	}
+	inline Score& operator *= (const short& s) {
+		middle_game = short(middle_game * s);
+		end_game = short(end_game * s);
+		return *this;
+	}
+};
+
+typedef Score S;
