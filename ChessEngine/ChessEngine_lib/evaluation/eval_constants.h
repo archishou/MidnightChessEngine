04a6bef0662cf313a4d59834e970675472aa7325
Archishmaan <archishou@gmail.com>
2023-03-08 12:29:40 -0500

    fixup changelog and reorg constants

--- ChessEngine/ChessEngine_lib/evaluation/eval_constants.h
+++ ChessEngine/ChessEngine_lib/evaluation/eval_constants.h
@@ -5,16 +5,16 @@
 // Created by Archishmaan Peyyety on 1/10/23.
 //
 
+// If this is updated be sure compute_game_phase loop is also updated. Currently only checks non-zeros.
+constexpr int GAME_PHASE_BONUS[NPIECE_TYPES] = {
+		0, 1, 1, 2, 4, 0
+};
 // Pawn Bonuses
 constexpr Score PASSED_PAWN_BONUS = S(1, 17);
 constexpr Score ISOLATED_PAWN_PENALTY = S(-23, -13);
 // Bishop Bonuses
 constexpr Score BISHOP_PAIR_BONUS =S(32, 54);
 
-// If this is updated be sure compute_game_phase loop is also updated. Currently only checks non-zeros.
-constexpr int GAME_PHASE_BONUS[NPIECE_TYPES] = {
-		0, 1, 1, 2, 4, 0
-};
 
 constexpr Score PIECE_VALUES[NPIECE_TYPES] = {
 		S(107, 80), S(412, 307), S(399, 306), S(538, 564), S(1282, 948), S(0, 0),
@@ -47,6 +47,11 @@ constexpr Score KING_TABLE[NSQUARES] = {
 		S(-94, -78), S(183, -71), S(181, -55), S(102, -42), S(-149, 15), S(-78, 29), S(49, -3), S(43, -25), S(230, -59), S(65, 7), S(37, 9), S(117, -2), S(34, 14), S(44, 35), S(-27, 31), S(-149, 40), S(76, -6), S(76, 10), S(96, 11), S(24, 13), S(41, 12), S(120, 33), S(128, 28), S(-12, 13), S(11, -19), S(-11, 24), S(17, 26), S(-53, 40), S(-54, 38), S(-54, 46), S(-18, 33), S(-95, 16), S(-92, -9), S(16, -6), S(-74, 37), S(-132, 51), S(-139, 54), S(-96, 43), S(-86, 25), S(-109, 4), S(-8, -23), S(-25, 2), S(-61, 25), S(-98, 39), S(-96, 43), S(-85, 35), S(-36, 14), S(-54, -2), S(-3, -35), S(2, -12), S(-46, 16), S(-102, 28), S(-80, 28), S(-55, 18), S(-1, -5), S(8, -26), S(-27, -66), S(41, -51), S(11, -27), S(-96, 1), S(-14, -22), S(-49, -7), S(31, -40), S(18, -65)
 };
 
+constexpr Score KNIGHT_MOBILITY = S(1, 0);
+constexpr Score BISHOP_MOBILITY = S(6, 4);
+constexpr Score ROOK_MOBILITY = S(8, 2);
+constexpr Score QUEEN_MOBILITY = S(2, 8);
+
 // TODO: This kinda slow. Like 8 conditionals when it could just be PST[COLOR][TYPE][SQUARE]
 template<Color color>
 constexpr Score read_psqt(PieceType piece_type, Square square) {
@@ -61,8 +66,3 @@ constexpr Score read_psqt(PieceType piece_type, Square square) {
 		default: return {};
 	}
 }
-
-constexpr Score KNIGHT_MOBILITY = S(1, 0);
-constexpr Score BISHOP_MOBILITY = S(6, 4);
-constexpr Score ROOK_MOBILITY = S(8, 2);
-constexpr Score QUEEN_MOBILITY = S(2, 8);
