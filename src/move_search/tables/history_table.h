6d1134669c8c5688be7f3f0fbcee2c4002c95b66
Alexander Tian <alex2262alexander@gmail.com>
2023-04-08 15:40:25 -0400

    move ordering fix with quiet definition.

--- src/move_search/tables/history_table.h
+++ src/move_search/tables/history_table.h
@@ -20,7 +20,7 @@ extern void update_history_entry(int& history_entry, int bonus);
 
 template<Color color>
 void update_history(ScoredMoves& ordered_moves, Move &best_move, int depth, int ply, int move_idx) {
-	if (best_move.flag() != QUIET) return;
+	if (!best_move.is_quiet()) return;
 
 	killers[ply][1] = killers[ply][0];
 	killers[ply][0] = best_move;
@@ -31,7 +31,7 @@ void update_history(ScoredMoves& ordered_moves, Move &best_move, int depth, int 
 
 	for (int idx = 0; idx < move_idx; idx++) {
 		const Move& move = ordered_moves[idx].move;
-		if (move.flag() != QUIET) continue;
+		if (!move.is_quiet()) continue;
 		update_history_entry(history[color][move.from()][move.to()], -history_bonus);
 	}
 }
