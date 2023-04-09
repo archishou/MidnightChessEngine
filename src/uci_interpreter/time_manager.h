3cbf0d72af954f10182de99244a953bf97f2c3ec
Archishmaan <archishou@gmail.com>
2023-04-09 18:49:13 -0400

    use 0.95 instead of 0.8

--- src/uci_interpreter/time_manager.h
+++ src/uci_interpreter/time_manager.h
@@ -6,7 +6,7 @@ int time_iterative_deepening(int time_remaining, int increment, int moves_to_go)
 	if (moves_to_go != -1) {
 		moves_to_go = std::min(moves_to_go, 50);
 		double scale = 0.7 / moves_to_go;
-		double eight = 0.8 * time_remaining;
+		double eight = 0.95 * time_remaining;
 		return static_cast<int>(std::min(scale * time_remaining, eight));
 	}
 	return (time_remaining / 10) + (increment / 2);
@@ -16,7 +16,7 @@ int time_search(int time_remaining, int increment, int moves_to_go) {
 	if (moves_to_go != -1) {
 		moves_to_go = std::min(moves_to_go, 50);
 		double scale = 0.7 / moves_to_go;
-		double eight = 0.8 * time_remaining;
+		double eight = 0.95 * time_remaining;
 		int opt_time = static_cast<int>(std::min(scale * time_remaining, eight));
 		return static_cast<int>(std::min(5.0 * opt_time, eight));
 	}
