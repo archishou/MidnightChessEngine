8a0c5b790ecf57c096a262f9d8810aba78d9b2ec
Archishmaan <archishou@gmail.com>
2023-04-11 16:48:35 -0400

    bench as command line arg

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "uci_interpreter/uci_interpreter.h"
 
-int main () {
+int main (int argc, char *argv[]) {
+	initialize_engine();
+	if (argc > 1 && std::string(argv[1]) == "bench") bench();
 	read_uci();
 }
