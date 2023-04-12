6b82762d7b8cb0763b1583b90461e3fe8031cc35
Archishmaan <archishou@gmail.com>
2023-04-11 22:08:55 -0400

    exit after bench
    Bench: 31918717

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 
 int main (int argc, char *argv[]) {
 	initialize_engine();
-	if (argc > 1 && std::string(argv[1]) == "bench") bench();
+	if (argc > 1 && std::string(argv[1]) == "bench") {
+		bench();
+		return 0;
+	}
 	read_uci();
 }
