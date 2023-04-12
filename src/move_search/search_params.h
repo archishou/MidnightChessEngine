ee4e114c5da4ce017d6cc8d9bb70cf6f1fec5ba1
Archishmaan <archishou@gmail.com>
2023-04-12 18:57:42 -0400

    even more aggressiev lmr
    Bench: 21831255

--- src/move_search/search_params.h
+++ src/move_search/search_params.h
@@ -5,8 +5,8 @@
 const int RFP_MARGIN = 75;
 const int RFP_MAX_DEPTH = 9;
 
-const double LMR_BASE = 1.0;
-const double LMR_DIVISOR = 2.0;
+const double LMR_BASE = 1.2;
+const double LMR_DIVISOR = 1.8;
 
 const int NMP_MIN_DEPTH = 3;
 
