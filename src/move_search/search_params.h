25693aebff9b51c61e617309902fc616697cdaf0
Archishmaan <archishou@gmail.com>
2023-04-11 22:10:25 -0400

    Try new LMR Constants
    Bench: 26029471

--- src/move_search/search_params.h
+++ src/move_search/search_params.h
@@ -5,8 +5,8 @@
 const int RFP_MARGIN = 75;
 const int RFP_MAX_DEPTH = 9;
 
-const double LMR_BASE = 0.8;
-const double LMR_DIVISOR = 2.2;
+const double LMR_BASE = 1.0;
+const double LMR_DIVISOR = 2.0;
 
 const int NMP_MIN_DEPTH = 3;
 
