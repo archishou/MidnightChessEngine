616acc039f4e80264ea8145b9acacc9436e73411
Archishmaan <archishou@gmail.com>
2023-04-15 19:26:45 -0400

    refactor search.h

---
+++ src/move_search/types.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Archishmaan Peyyety on 4/15/23.
+//
+#include "types.h"
+std::ostream& operator<<(std::ostream& os, const Line& line) {
+	for(const Move & i : line) {
+		if (i == 0) break;
+		os << i << " ";
+	}
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const BestMoveSearchResults& results) {
+	os << "Best Move: " << results.best_move << std::endl;
+	os << "Principal Variation: " << results.pv << std::endl;
+	os << "Depth Searched: " << results.depth_searched << std::endl;
+	os << "Seldepth: " << results.seldepth << std::endl;
+	os << "Evaluation: " << results.value << std::endl;
+	os << "Time Searched: " << results.time_searched << std::endl;
+	os << "Nodes Searched: " << results.nodes_searched << std::endl;
+	os << "Node / Second: " << results.nodes_per_second << std::endl;
+	os << "Q Nodes Searched: " << results.q_nodes_searched << std::endl;
+	return os;
+}
