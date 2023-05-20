17c3793c5dbf82c122f40b6e9c9b6fda15e6eb2b
Archishmaan <archishou@gmail.com>
2023-05-20 15:17:32 -0400

    .cpp in midnight lib

--- src/engine.cpp
+++ src/engine.cpp
@@ -2,6 +2,8 @@
 // Created by Archishmaan Peyyety on 3/20/23.
 //
 #include "engine.h"
+#include "move_search/move_ordering/move_ordering.h"
+#include "move_search/tables/lmr_table.h"
 
 void initialize_engine() {
 	initialize_move_sort_tables();
--- src/engine.h
+++ src/engine.h
@@ -1,5 +1,3 @@
 #pragma once
-#include "move_search/move_ordering/move_ordering.h"
-#include "move_search/tables/lmr_table.h"
 
 void initialize_engine();
