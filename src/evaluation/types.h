872ce72eb7a125c58c8554aa86030283ff3ddf36
Archishmaan <archishou@gmail.com>
2023-05-19 23:49:52 -0400

    trace

--- src/evaluation/types.h
+++ src/evaluation/types.h
@@ -5,6 +5,7 @@
 
 #include <cstring>
 #include <cstdint>
+#include "board/types/piece.h"
 
 typedef int Score;
 constexpr Score SCORE_ZERO = 0;
@@ -30,3 +31,42 @@ inline int mg_value(Score s) {
 
 	return static_cast<int>(v);
 }
+
+struct Trace {
+	int score={};
+
+	short material[NPIECE_TYPES][NCOLORS]{};
+
+	short pawn_pst[NSQUARES][NCOLORS]{};
+	short knight_pst[NSQUARES][NCOLORS]{};
+	short bishop_pst[NSQUARES][NCOLORS]{};
+	short rook_pst[NSQUARES][NCOLORS]{};
+	short queen_pst[NSQUARES][NCOLORS]{};
+	short king_pst[NSQUARES][NCOLORS]{};
+
+	short open_files[NPIECE_TYPES][NCOLORS]{};
+	short semi_open_files[NPIECE_TYPES][NCOLORS]{};
+	short pawn_protection[NPIECE_TYPES][NCOLORS]{};
+	short attacked_by_pawn[NPIECE_TYPES][NCOLORS]{};
+	short threats[NPIECE_TYPES * NPIECE_TYPES][NCOLORS]{};
+	short king_ring_bonus[NPIECE_TYPES][NCOLORS]{};
+	short check_bonus[NPIECE_TYPES][NCOLORS]{};
+	short center_control[NPIECE_TYPES][NCOLORS]{};
+
+	short king_pawn_shield[2][NCOLORS]{};
+	short passed_pawns[NSQUARES][NCOLORS]{};
+	short blocked_passed_pawns[NSQUARES][NCOLORS]{};
+	short isolated_pawns[NCOLORS]{};
+	short doubled_pawns[NCOLORS]{};
+	short bishop_bonus[NCOLORS]{};
+
+	short knight_mobility[9][NCOLORS]{};
+	short bishop_mobility[14][NCOLORS]{};
+	short rook_mobility[15][NCOLORS]{};
+	short queen_mobility[28][NCOLORS]{};
+
+	short pawn_phalanx[NRANKS][NCOLORS]{};
+	short candidate_pawns[NRANKS][NCOLORS]{};
+
+	short tempo[NCOLORS]{};
+};
