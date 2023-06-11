d905f184b8e854dcc8e65ec3e84365b9219c8c7e
Archishmaan <archishou@gmail.com>
2023-06-11 01:17:59 -0400

    Refactor Time Manager
    STC
    https://chess.swehosting.se/test/1546/
    ELO   | -1.00 +- 5.03 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=64MB
    LLR   | 0.41 (-2.94, 2.94) [-5.00, 0.00]
    GAMES | N: 8376 W: 1905 L: 1929 D: 4542
    
    LTC
    https://chess.swehosting.se/test/1547/
    ELO   | 2.21 +- 8.85 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 0.55 (-2.94, 2.94) [-5.00, 0.00]
    GAMES | N: 2200 W: 417 L: 403 D: 1380
    
    MTG
    https://chess.swehosting.se/test/1548/
    ELO   | -1.61 +- 4.60 (95%)
    SPRT  | 40/4.0+0.00s Threads=1 Hash=64MB
    LLR   | 0.24 (-2.94, 2.94) [-5.00, 0.00]
    GAMES | N: 11648 W: 3072 L: 3126 D: 5450
    
    Bench: 13974136

---
+++ src/uci_interpreter/time_manager.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Archishmaan Peyyety on 6/10/23.
+//
+#include "time_manager.h"
+
+TimeBounds allocate_time(int time_remaining, int increment, int moves_to_go) {
+	TimeBounds time_bounds{};
+	if (moves_to_go == -1) time_bounds = allocate_time_standard(time_remaining, increment);
+	else time_bounds = allocate_time_moves_to_go(time_remaining, moves_to_go);
+	return time_bounds;
+}
+
+TimeBounds allocate_time_standard(int time_remaining, int increment) {
+	TimeBounds bounds{};
+	bounds.first = (time_remaining / 40) + (3 * increment / 4);
+	bounds.second = (time_remaining / 10) + (3 * increment / 4);
+	return bounds;
+}
+
+TimeBounds allocate_time_moves_to_go(int time_remaining, int moves_to_go) {
+	moves_to_go = std::min(moves_to_go, 50);
+	double scale = 0.7 / moves_to_go;
+	double eight = 0.95 * time_remaining;
+	int optimum = static_cast<int>(std::min(scale * time_remaining, eight));
+
+	TimeBounds bounds{};
+	bounds.first = static_cast<int>(std::min(5.0 * optimum, eight));
+	bounds.second = optimum;
+	return bounds;
+}
+
